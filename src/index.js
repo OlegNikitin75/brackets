877fdd200e74abc8740b46bab068e8f88b42975c
Aleh Nikitsin <nov075@mail.ru>
2021-08-01 14:38:41 +0300

    feat: added a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let arrStr = str.split('');
+	let arrOpenBr = [];
+	let result = true;
+
+	arrStr.forEach(element => {
+		bracketsConfig.forEach(item => {
+			if (element === item[0] && element === item[1]) {
+				if (element === arrOpenBr[arrOpenBr.length - 1]) {
+					arrOpenBr.pop()
+				}
+				else {
+					arrOpenBr.push(element);
+				}
+			} else if (element === item[0]) {
+				arrOpenBr.push(element);
+			} else if (element === item[1]) {
+				if (item[0] === arrOpenBr[arrOpenBr.length - 1] && arrOpenBr[0] !== undefined) {
+					arrOpenBr.pop()
+				} else result = false
+			}
+		});
+	})
+	if (arrOpenBr.length === 0 && result)
+		return true
+	return false
 }
